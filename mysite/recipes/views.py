cb73699b3dde61e7b745786097c4b82ce54b2f4b
Wilhelmina Vanderpool <wv0996337@otc.edu>
2026-01-29 15:01:33 -0600

    Add pagination

--- mysite/recipes/views.py
+++ mysite/recipes/views.py
@@ -1,13 +1,32 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import Http404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 
 # Post list view
 def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'recipes/post/list.html', {'posts': posts})
+    post_qs = Post.published.all()
+    paginator = Paginator(post_qs, 5)  # 5 posts per page
+    page = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage: 
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request, 'recipes/post/list.html', {
+        'posts': posts,
+    })
 
 # Post detail view
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post,
+        slug=post,
+        status=Post.Status.PUBLISHED,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+    )
     return render(request, 'recipes/post/detail.html', {'post': post})
